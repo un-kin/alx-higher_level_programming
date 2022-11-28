f73b7b9ad564bc57986d484bf83187f38a8a61a3
Tobikiller <edwardnguimeya@yahoo.com>
2022-11-28 15:04:22 -0800

    7edeges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word
+word_last_2 = word
+middle_word = word
+print(f"First 3 letters: {word_first_3[0:3]}")
+print(f"Last 2 letters: {word_last_2[7:9]}")
+print(f"Middle word: {middle_word[1:8]}")
