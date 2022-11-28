52c51e72c8f5d7959eea6b9d92f75c6136083873
Tobikiller <edwardnguimeya@yahoo.com>
2022-11-28 15:57:38 -0800

    7-edeg fixed

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word
-word_last_2 = word
-middle_word = word
-print(f"First 3 letters: {word_first_3[0:3]}")
-print(f"Last 2 letters: {word_last_2[7:9]}")
-print(f"Middle word: {middle_word[1:-1]}")
+word_first_3 = word[0:3]
+word_last_2 = word[7:9]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
