9f2f5da052843cef516533d6953fce4e7da18311
Tobikiller <edwardnguimeya@yahoo.com>
2022-11-28 15:55:03 -0800

    7-edeges fixed

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,4 +5,4 @@ word_last_2 = word
 middle_word = word
 print(f"First 3 letters: {word_first_3[0:3]}")
 print(f"Last 2 letters: {word_last_2[7:9]}")
-print(f"Middle word: {middle_word[1:8]}")
+print(f"Middle word: {middle_word[1:-1]}")
