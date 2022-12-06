f5c3b2e7db837357964f446c73acc19e3659e21f
Tobikiller <edwardnguimeya@yahoo.com>
2022-12-06 19:08:52 +0000

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
+    else:
+        print("{} argument:".format(args))
+
+    if args >= 1:
+
+        args = 0
+        for args_str in sys.argv:
+            if args != 0:
+                print("{}: {}".format(args, args_str))
+            args += 1
