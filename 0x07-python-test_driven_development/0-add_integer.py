22e3d06358254d439ef9cf0a6f20498cf5c53f34
Tobikiller <edwardnguimeya@yahoo.com>
2023-01-02 17:31:04 +0000

    files added for testing

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Describes a function to adding two integers
+"""
+
+
+def add_integer(a, b=98):
+    """Take the arguments to add.
+    a: One of the numbers, default not defined.
+    b: The other number, default 98.
+    Return: int(a + b)
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
