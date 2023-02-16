7860dbef564a41787cdb286ae8c86449c7e68cb5
Tobikiller <edwardnguimeya@yahoo.com>
2023-02-16 11:45:03 -0800

    errors checked

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Insert a new row in the table
-INSERT INTO first_table(id, name) VALUES(89, 'Coding School');
+-- Inserts a new row in the table
+INSERT INTO first_table(id, name) VAlUES(89, 'Coding School');
