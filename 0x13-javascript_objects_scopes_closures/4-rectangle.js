d317d46d23988256a684677e4865353352adc030
Tobikiller <edwardnguimeya@yahoo.com>
2023-03-15 09:11:18 -0700

    4 rectngle semistandard correction

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,26 @@
 #!/usr/bin/node
-// JS script to define a class, creates an instance method to print rectangle
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let count = 0; count < this.height; count++) {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
-    let holder = this.width;
+    const tmp = this.width;
     this.width = this.height;
-    this.height = holder;
+    this.height = tmp;
   }
+
   double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
+    this.width *= 2;
+    this.height *= 2;
   }
-}
-module.exports = Rectangle;
+};
